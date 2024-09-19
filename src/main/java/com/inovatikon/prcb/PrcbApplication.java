fcbf6afbc90d9be5e7401342c3a9f1c5ce9ff27c
viks91 <viktor.stefanovski@inovatikon.mk>
2024-09-19 16:04:27 +0200

    first commit

---
+++ src/main/java/com/inovatikon/prcb/PrcbApplication.java
@@ -0,0 +1,13 @@
+package com.inovatikon.prcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrcbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovatikon/prcb/PrcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovatikon.prcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
